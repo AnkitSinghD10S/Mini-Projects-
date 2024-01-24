e9babecd2e915c5c9ebb41b837d7a28f1d51c7df
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-01-24 22:27:24 +0530

    Notesapp

--- Notes App/NotesAPI.js
+++ Notes App/NotesAPI.js
@@ -5,24 +5,28 @@ export default class NotesAPI{
         return notes;
     }
 
-    static saveNote(noteToSave){
+    static saveNote(noteToSave) {
         const notes = NotesAPI.getAllNotes();
-        const existing =notes.find(note => note.id == noteToSave.id);
+        const existing = notes.find(note => note.id == noteToSave.id);
 
-        if(existing){
+        // Edit/Update
+        if (existing) {
             existing.title = noteToSave.title;
-            existing.body= noteToSave.body;
-            existing.updated = new Date().toISOString();;
-        }else{
-            noteToSave.id= Math.floor(Math.random() *1000000);
-            noteToSave.updated= new Date().toISOString();
+            existing.body = noteToSave.body;
+            existing.updated = new Date().toISOString();
+        } else {
+            noteToSave.id = Math.floor(Math.random() * 1000000);
+            noteToSave.updated = new Date().toISOString();
             notes.push(noteToSave);
         }
 
-        localStorage.setItem("notesapp-notes",JSON.stringify(notes));
+        localStorage.setItem("notesapp-notes", JSON.stringify(notes));
     }
 
     static deleteNote(id){
+        const notes = NotesAPI.getAllNotes();
+        const newNotes = notes.filter(note => note.id != id);
 
+        localStorage.setItem("notesapp-notes", JSON.stringify(newNotes));
     }
 }
--- Notes App/index.js
+++ Notes App/index.js
@@ -2,8 +2,10 @@ import NotesAPI from "./NotesAPI.js"
 
 NotesAPI.saveNote({
     id: 857990,
-    title: "New note ram!", 
-    body: "I am a new notes."
-});+
+    title: "Neew notes!", 
+    body: "the new note is created"
+});
+
+// NotesAPI.deleteNote(857990);
 
 console.log(NotesAPI.getAllNotes());
